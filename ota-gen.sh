6cf2bf13d8af2e6662f0cf0286f056616d8893e8
Akhil Narang <akhilnarang.1999@gmail.com>
2018-03-05 19:35:30 +0530

    OTA Update for RR oneplus3 20180305 Build
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- ota-gen.sh
+++ ota-gen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 device=$1
-cd ~/rr/ota/akhil
-oldzip=$(grep "<Filename>RR-N" $device.xml  | sed -e 's|</\?Filename>||g')
+cd ~/rr-o/ota/akhil
+oldzip=$(grep "<Filename>RR-O" $device.xml  | sed -e 's|</\?Filename>||g')
 newzip=$2
 sed -i "s/$oldzip/$newzip/g" $device.xml
 cd -
-cd ~/rr/ota
+cd ~/rr-o/ota
 echo "OTA Update for RR $device $(date +%Y%m%d) Build" > /tmp/rrota
 git commit -as -F /tmp/rrota
 git pull --no-edit origin master;
